a0d1433ba68f1fe9a0ba1cdb6e9cda4d161e7f1d
Kalp <kalpshah2003@gmail.com>
2021-08-19 11:14:16 -0400

    form template

--- client_side/src/App.js
+++ client_side/src/App.js
@@ -1,22 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import Typography from '@material-ui/core/Typography';
+import Button from "@material-ui/core/Button"
+import { useState } from "react";
 
 function App() {
+
+  const [userName, setUserName] = useState('');
+  const [userHeight, setUserHeight] = useState('0');
+  const [userWeight, setUserWeight] = useState('0');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const userInformation = { userName, userHeight, userWeight };
+
+    fetch('http://localhost:8000/userData', {
+      method: 'POST',
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(userInformation)
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="form">
+        <br></br>
+        <Typography variant="h4" align="center">
+          Personal Information
+        </Typography>
+        
+        <br></br>
+        <form onSubmit={handleSubmit}>
+          <label>Name:</label>
+          <input type="text" required value={userName} onChange={(e) => setUserName(e.target.value)}></input>
+          <br></br>
+          <label>Height (cm):</label>
+          <input type="number" required value={userHeight} onChange={(e) => setUserHeight(e.target.value)}></input>
+          <br></br>
+          <label>Weight (kg):</label>
+          <input type="number" required value={userWeight} onChange={(e) => setUserWeight(e.target.value)}></input>
+
+          <br></br>
+          <Button variant="contained" align="center">Submit</Button>
+        </form>
       </header>
     </div>
   );
