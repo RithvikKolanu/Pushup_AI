109ea8fd81979dd6348603b043c798887119020e
RithvikKolanu <42521538+RithvikKolanu@users.noreply.github.com>
2021-08-19 14:46:29 -0400

    app server start

--- server_side/app.py
+++ server_side/app.py
@@ -1,1 +1,19 @@
 from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+@app.route('/')
+def main():
+    return("helloworld")
+
+class HelloWorld(Resource):
+    def get(self, name):
+        return {"name": name}
+
+
+api.add_resource(HelloWorld, "/helloworld/<string:name>")
+
+if __name__ == ("__main__"):
+    app.run(debug=True)
