4b94194c6af68fc79aba5fd89987b2f4bd00110e
RithvikKolanu <42521538+RithvikKolanu@users.noreply.github.com>
2021-08-19 15:27:24 -0400

    data

---
+++ server_side/splitting_images.py
@@ -0,0 +1,20 @@
+import cv2
+
+capture = cv2.VideoCapture('c:/projects/workout_app/server_side/Content/good_form.mp4')
+framenr = 0
+
+while(True):
+    success, frame = capture.read()
+    print("worked")
+
+    if success:
+        cv2.imwrite(f'c:/projects/workout_app/server_side/Content/al/{framenr}.jpg', frame)
+        print("success")
+    else:
+        print('failed')
+        break
+
+    framenr = framenr+1
+    print(framenr)
+
+capture.release() 
