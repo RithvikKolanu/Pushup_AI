0a2f8a2f54f28623850c76172500d0100db03ccf
RithvikKolanu <42521538+RithvikKolanu@users.noreply.github.com>
2021-08-20 18:11:27 -0400

    comments

--- server_side/load.py
+++ server_side/load.py
@@ -7,7 +7,10 @@ from keras.models import load_model
 model = load_model('c:/projects/workout_app/server_side/model.h5')
 path = "c:/projects/workout_app/server_side/content"
 
+
+#load the react image in the first path here
 test_image = image.load_img(path+"/validation/good/1635.jpg", target_size=[64, 64])
+
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
 result = model.predict(test_image)
