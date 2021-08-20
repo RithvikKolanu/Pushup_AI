242dd5625208df7b32545fbb298f15b90b782380
RithvikKolanu <42521538+RithvikKolanu@users.noreply.github.com>
2021-08-20 18:13:56 -0400

    fixed the h5 file

--- server_side/load.py
+++ server_side/load.py
@@ -9,7 +9,7 @@ path = "c:/projects/workout_app/server_side/content"
 
 
 #load the react image in the first path here
-test_image = image.load_img(path+"/validation/good/1635.jpg", target_size=[64, 64])
+test_image = image.load_img(path+"/validation/bad/1455.jpg", target_size=[64, 64])
 
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
