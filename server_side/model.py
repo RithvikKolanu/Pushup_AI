f1e7a137322a5363308b593584f7547f5fab7e51
RithvikKolanu <42521538+RithvikKolanu@users.noreply.github.com>
2021-08-20 17:14:21 -0400

    Model

--- server_side/model.py
+++ server_side/model.py
@@ -6,6 +6,7 @@ import os
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.optimizers import RMSprop
+from tensorflow.python.keras.backend import conv2d
 
 #this is the path for the pictures
 #make this a relative path
@@ -13,13 +14,41 @@ path = "c:/projects/workout_app/server_side/content"
 
 print(cv2.imread(path + "/training/good/3.jpg").shape)
 
-#converts the images to black and white
-train = ImageDataGenerator(rescale = 1/255)
-test = ImageDataGenerator(rescale = 1/255)
-validation = ImageDataGenerator(rescale = 1/255)
+#Transformations for the images
+train = ImageDataGenerator(rescale = 1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
+test = ImageDataGenerator(rescale = 1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
+validation = ImageDataGenerator(rescale = 1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
 
-train_dataset = train.flow_from_directory(path+"/training", target_size = (480, 270), batch_size = 36, class_mode = 'binary', )
-test_dataset = test.flow_from_directory(path+"/testing", target_size = (480, 270), batch_size = 36, class_mode = 'binary', )
-validation_dataset = validation.flow_from_directory(path+"/validation", target_size = (480, 270), batch_size = 36, class_mode = 'binary', )
+#gets the images from directory, resizes them, defining classes and groups(good and bad pushups)
+train_dataset = train.flow_from_directory(path+"/training", target_size = (64, 64), batch_size = 32, class_mode = 'binary', )
+test_dataset = test.flow_from_directory(path+"/testing", target_size = (64, 64), batch_size = 32, class_mode = 'binary', )
+validation_dataset = validation.flow_from_directory(path+"/validation", target_size = (64, 64), batch_size = 32, class_mode = 'binary', )
 
-print(train_dataset[0])
+print(train_dataset.class_indices)
+
+#initializing the model
+cnn = tf.keras.models.Sequential()
+
+#adding a filter to the CNN
+cnn.add(tf.keras.layers.Conv2d(filters=32, kernel_size = 3, activation="relu", input_shape=[64, 64, 3]))
+
+#Pooling layer -> makes the convolution layers smaller
+cnn.add(tf.keras.layers.MaxPool2d(pool_size = 2, strides=2))
+
+#Second filter
+cnn.add(tf.keras.layers.Conv2d(filters=32, kernel_size = 3, activation="relu"))
+
+#Second pooling layer
+cnn.add(tf.keras.layers.MaxPool2d(pool_size = 2, strides=2))
+
+#Flattening layer
+cnn.add(tf.keras.layers.Flatten())
+
+#Connection
+cnn.add(tf.keras.layers.Dense(units = 128, activation = "relu", ))
+
+#Output
+cnn.add(tf.keras.layers.Dense(units = 1, activation = "sigmoid"))
+
+
+
